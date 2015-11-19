5b795f31b66150ad3a39281f3a19c4c64006ef66
Desbouche Christophe <cdesbouche@acipia.com>
2015-11-19 17:36:06 +0100

    Force last compatibility for IE
    IE (tested with IE11), by default, will use the compatibility mode IE5 (~o~)
    Added meta tag for edge compatibility.

--- templates/header.php
+++ templates/header.php
@@ -9,6 +9,7 @@
 
 		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 		<?php
 		wp_print_styles( array( 'presenter', 'reveal', 'reveal-theme', 'reveal-lib-zenburn' ) );
