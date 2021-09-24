3b0f12b2b2439887d9fdcad427a79d2c2e51a5ed
Aaron D. Campbell <Aaron@AaronDCampbell.com>
2021-09-24 13:55:53 -0500

    Split save and edit methods into their own js files

--- src/index.js
+++ src/index.js
@@ -1,206 +1,24 @@
 // SCSS Files to process & build
 import './index.scss'
 
-import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
-import { InnerBlocks } from '@wordpress/block-editor';
-import {
-	useBlockProps,
-	ColorPalette,
-	InspectorControls,
-	MediaUpload,
-	MediaUploadCheck
-} from '@wordpress/block-editor';
-import {
-	Panel,
-	PanelBody,
-	TextareaControl,
-	ToggleControl,
-	TextControl,
-	Button,
-	Spinner,
-	ResponsiveWrapper,
-} from '@wordpress/components';
-import { cleanForSlug } from '@wordpress/url';
-import { useSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import edit from './edit';
+import save from './save';
 import metadata from '../block.json'
 
-const ALLOWED_MEDIA_TYPES = ['image'];
-
 registerBlockType( metadata, {
-	edit( props ) {
-		const {
-			attributes: { title, speakerNotes, hidden, bgColor, bgImageId },
-			setAttributes,
-		} = props;
+	/**
+	 * @see ./edit.js
+	 */
+	 edit: edit,
 
-		// Get the background image id there is one
-		const { bgImage } = useSelect( ( select ) => {
-			return {
-				bgImage: bgImageId ? select( 'core' ).getMedia( bgImageId ) : null,
-			};
-		});
+	/**
+	 * @see ./save.js
+	 */
+	 save: save,
 
-		const blockProps = useBlockProps();
-
-		let slideStyles = {
-			backgroundColor: bgColor || undefined,
-			backgroundImage: ( bgImage && bgImage.source_url )? `url( ${ bgImage.source_url } )` : undefined,
-			backgroundRepeat: ( bgImage && bgImage.source_url )? 'no-repeat' : undefined,
-			backgroundPosition: ( bgImage && bgImage.source_url )? 'center' : undefined,
-			backgroundSize: ( bgImage && bgImage.source_url )? 'cover' : undefined,
-		};
-
-		const instructions = (
-			<p>
-				{ __( 'To edit the background image, you need permission to upload media.', 'presenter' ) }
-			</p>
-		);
-
-		const onChangeTitle = ( value ) => {
-			setAttributes( { title: value } );
-		};
-		const onChangeSpeakerNotes = ( value ) => {
-			setAttributes( { speakerNotes: value } );
-		};
-		const onChangeHidden = ( value ) => {
-			setAttributes( { hidden: value } );
-		};
-		const onChangeBGColor = ( value ) => {
-			setAttributes( { bgColor: value } );
-		};
-		const onUpdateImage = ( image ) => {
-			setAttributes( {
-				bgImageId: image.id,
-				bgImageUrl: image.url,
-			} );
-		};
-		const onRemoveImage = () => {
-			setAttributes( {
-				bgImageId: undefined,
-				bgImageUrl: undefined,
-			} );
-		};
-
-		return (
-			<div { ...blockProps }>
-				<InspectorControls key="setting">
-					<Panel>
-						<PanelBody title={ __('Background Color', 'presenter') } icon='art' initialOpen={false}>
-							<ColorPalette
-								onChange={ onChangeBGColor }
-								value={ bgColor }
-							/>
-						</PanelBody>
-					</Panel>
-					<Panel>
-						<PanelBody title={__('Background Image', 'presenter')} icon='format-image' initialOpen={false}>
-							<div className="presenter-background-image-selector">
-								<MediaUploadCheck fallback={instructions}>
-									<MediaUpload
-										title={ __('Background image', 'presenter') }
-										onSelect={onUpdateImage}
-										allowedTypes={ ALLOWED_MEDIA_TYPES }
-										value={bgImageId}
-										render={ ( { open  } ) => (
-											<Button
-												className={ ! bgImageId ? 'editor-post-featured-image__toggle' : 'editor-post-featured-image__preview' }
-												onClick={ open }>
-												{ ! bgImageId && ( __( 'Set background image', 'presenter' ) ) }
-												{ !! bgImageId && ! bgImage && <Spinner /> }
-												{ !! bgImageId && bgImage &&
-													<ResponsiveWrapper
-														naturalWidth={ bgImage.media_details.width }
-														naturalHeight={ bgImage.media_details.height }
-													>
-														<img src={ bgImage.source_url } alt={ __( 'Background image', 'presenter' ) } />
-													</ResponsiveWrapper>
-												}
-											</Button>
-										)}
-									/>
-									{ !! bgImageId && bgImage &&
-										<MediaUpload
-											title={ __( 'Background image', 'image-selector-example' ) }
-											onSelect={ onUpdateImage }
-											allowedTypes={ ALLOWED_MEDIA_TYPES }
-											value={ bgImageId }
-											render={ ( { open } ) => (
-												<Button onClick={ open } isSecondary>
-													{ __( 'Replace Image', 'presenter' ) }
-												</Button>
-											) }
-										/>
-									}
-									{ !! bgImageId &&
-										<Button onClick={ onRemoveImage } isLink isDestructive>
-											{ __( 'Remove background image', 'presenter' ) }
-										</Button>
-									}
-								</MediaUploadCheck>
-							</div>
-						</PanelBody>
-					</Panel>
-					<Panel>
-						<PanelBody title={ __('Slide Name', 'presenter') } initialOpen={false}>
-							<TextControl
-								label={ __( 'Slide name', 'presenter' ) }
-								help={ __( 'This is sanitized and used as a hash in the URL.', 'presenter' ) }
-								onChange={ onChangeTitle }
-								value={ title }
-							/>
-						</PanelBody>
-					</Panel>
-					<Panel>
-						<PanelBody title={ __('Visibility', 'presenter') } icon='visibility' initialOpen={false}>
-							<ToggleControl
-								label={ __('Hidden', 'presenter') }
-								checked={ hidden }
-								onChange={ onChangeHidden }
-							/>
-						</PanelBody>
-					</Panel>
-				</InspectorControls>
-				<div className="reveal-viewport">
-					<div className="reveal">
-						<div className="slides">
-							<section className="presenter-slide" style={ slideStyles }>
-								<InnerBlocks />
-							</section>
-						</div>
-					</div>
-				</div>
-				<TextareaControl
-					label={ __( 'Speaker Notes', 'presenter' ) }
-					value={ speakerNotes }
-					onChange={ onChangeSpeakerNotes }
-				/>
-			</div>
-		);
-	},
-	save( props ) {
-		const {
-			attributes: { title, speakerNotes, hidden, bgColor, bgImageUrl },
-		} = props;
-
-		const TagName = hidden ? 'div' : 'section';
-
-		const blockProps = useBlockProps.save({
-			// Clean the Title and use it for the ID - Reveal.js uses this in a URL fragment
-			// If no title is specified use the block id to generate one - it is needed as an id for reveal.js
-			id: cleanForSlug( title || '' ),
-			style: {
-				display: hidden ? 'none' : undefined,
-			},
-			'data-background-color': bgColor || undefined,
-			'data-background-image': bgImageUrl || undefined,
-		});
-
-		return (
-			<TagName {...blockProps}>
-				<InnerBlocks.Content />
-				<aside className="notes">{speakerNotes}</aside>
-			</TagName>
-		);
-	},
 });
